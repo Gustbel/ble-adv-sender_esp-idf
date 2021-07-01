f4849856a0b19b2a9922b4a711bf44e2e5ad0847
Gustavo Belbruno <gustavobelbruno@gmail.com>
2021-07-01 08:40:34 -0300

    clean code

--- main/ble-sender-adv.c
+++ main/ble-sender-adv.c
@@ -16,33 +16,15 @@
 #include "esp_bt_main.h"
 #include "esp_bt_defs.h"
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_tls.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 
 static const char* DEMO_TAG = "ADV_DEV_DEMO";
-
-///Declare static functions
-static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
-esp_err_t status;
-
-
-static esp_ble_adv_params_t ble_adv_params = {
-    .adv_int_min        = 0x20,
-    .adv_int_max        = 0x40,
-    .adv_type           = ADV_TYPE_NONCONN_IND,
-    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
-    .channel_map        = ADV_CHNL_ALL,
-    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
-};
-
 
 uint8_t adv_data_raw[27] = {
           2, ESP_BLE_AD_TYPE_FLAG, (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT), // advertising flags
@@ -58,6 +40,19 @@ uint8_t adv_data_raw[27] = {
           0x36, 0x12,
           0x37, 0x12,
           0x38, 0x12};
+
+///Declare static functions
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+esp_err_t status;
+
+static esp_ble_adv_params_t ble_adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x40,
+    .adv_type           = ADV_TYPE_NONCONN_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
 
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {
@@ -90,7 +85,6 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
     }
 }
 
-
 void ble_adv_device_appRegister(void)
 {
     esp_err_t status;
@@ -102,7 +96,6 @@ void ble_adv_device_appRegister(void)
         ESP_LOGE(DEMO_TAG, "gap register error: %s", esp_err_to_name(status));
         return;
     }
-
 }
 
 void ble_adv_device_init(void)
@@ -116,43 +109,41 @@ void ble_adv_device_init(void)
 static void ble_update(void *pvParameters)
 {
 	while(1)
-	{	
+	{
 		adv_data_raw[11]++;
-		
-		printf("Data[11]: 0x%x\n", adv_data_raw[11]);
-				
-		esp_ble_gap_config_adv_data_raw((uint8_t*)&adv_data_raw, sizeof(adv_data_raw));
-		
-		vTaskDelay( (1000) / portTICK_PERIOD_MS);
+
+		printf("Data[11] updated, value: 0x%x -- ", adv_data_raw[11]);
+
+		status = esp_ble_gap_config_adv_data_raw((uint8_t*)&adv_data_raw, sizeof(adv_data_raw));
+
+		printf("Status: %d\n", status);
+
+		vTaskDelay( (10000) / portTICK_PERIOD_MS);
 	}
-	
     vTaskDelete(NULL);
 }
 
 void ble_adv_init()
 {
-	
 	ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
- esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+ 	esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     status = esp_bt_controller_init(&bt_cfg);
 	esp_bt_controller_enable(ESP_BT_MODE_BLE);
 
     ble_adv_device_init();
 
-
     if (status == ESP_OK){
         esp_ble_gap_config_adv_data_raw((uint8_t*)&adv_data_raw, sizeof(adv_data_raw));
     }
     else {
-        ESP_LOGE(DEMO_TAG, "Config iBeacon data failed: %s\n", esp_err_to_name(status));
+        ESP_LOGE(DEMO_TAG, "Config BLE Adv sender failed: %s\n", esp_err_to_name(status));
     }
 }
-
 
 
 void app_main(void)
 {
 	    ESP_ERROR_CHECK(nvs_flash_init());
 	    ble_adv_init();
-	    xTaskCreate(&ble_update, "update", 8192, NULL, 2, NULL);
+	    xTaskCreate(&ble_update, "update", 28192, NULL, 2, NULL);
 }
